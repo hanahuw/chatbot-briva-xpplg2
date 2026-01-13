b2c339919d0ea6e00657dba9c34a49618e4c16f4
hanahuw <hana.huwai@gmail.com>
2026-01-13 15:50:07 +0700

    import os

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import json
+import os
 
 app = Flask(__name__)
 
@@ -37,4 +38,5 @@ def chat():
         })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
